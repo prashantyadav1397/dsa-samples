a118543be85b902bded52e17cef8472937aaea2e
Prashant Yadav <41838500+prashantyadav1397@users.noreply.github.com>
2025-05-14 16:29:51 +0530

    Create disarium-number.ts

---
+++ disarium-number.ts
@@ -0,0 +1,16 @@
+// An example of this is the number 135:
+
+// There is a 1 in the first place.
+// There is a 3 in the second place.
+// There is a 5 in the third place.
+
+// (1^1 + 3^2 + 5^3) = 135
+// Input
+// Line 1: An integer, n.
+// Output
+// Line 1: Either Yes, n is a Disarium Number. or No, n is not a Disarium Number.
+
+// 89
+// 518
+// 135
+// 80
