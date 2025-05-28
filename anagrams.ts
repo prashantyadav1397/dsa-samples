34f696edf6ee88e311df3badd7f9e1d433d4550d
Prashant Yadav <41838500+prashantyadav1397@users.noreply.github.com>
2025-05-29 01:23:05 +0530

    Update anagrams.ts

--- anagrams.ts
+++ anagrams.ts
@@ -8,47 +8,59 @@
 // anagrams('RAIL! SAFETY!', 'fairy tales') === true
 // anagrams('Hi there', 'Bye there') === false
 
+// Approach 1 ==============================================
+// function cleanString(str: string) {
+//     return str.replace(/\W/g, '').toLowerCase();
+// }
+
+// function charMap(str: string) {
+//     return Array.from(str).reduce((acc: any, curr: string) => {
+//         const existing = acc[curr];
+
+//         if (existing) {
+//             acc[curr]++
+//         } else {
+//             acc[curr] = 1
+//         }
+
+//         return acc
+//     }, {})
+// }
+
+// function anagrams(stringA: string, stringB: string) {
+//     stringA = cleanString(stringA)
+//     stringB = cleanString(stringB)
+
+//     const charMapA = charMap(stringA);
+//     const charMapB = charMap(stringB);
+
+//     const keysA = Object.keys(charMapA);
+//     const keysB = Object.keys(charMapB);
+
+//     if (keysA.length === keysB.length) {
+//         return keysA.every((keyA) => {
+//             const keyB = keysB.find((keyB) => keyB === keyA)
+//             if (keyB && (charMapA[keyA] === charMapB[keyB])) {
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         })
+//     } else {
+//         return false
+//     }
+// }
+// =========================================================
+
+// Approach 2 ==============================================
 function cleanString(str: string) {
-    return str.replace(/\W/g, '').toLowerCase();
-}
-
-function charMap(str: string) {
-    return Array.from(str).reduce((acc: any, curr: string) => {
-        const existing = acc[curr];
-
-        if (existing) {
-            acc[curr]++
-        } else {
-            acc[curr] = 1
-        }
-
-        return acc
-    }, {})
+    return str.replace(/\W/g, '').toLowerCase().split('').sort().join('');
 }
 
 function anagrams(stringA: string, stringB: string) {
-    stringA = cleanString(stringA)
-    stringB = cleanString(stringB)
-
-    const charMapA = charMap(stringA);
-    const charMapB = charMap(stringB);
-
-    const keysA = Object.keys(charMapA);
-    const keysB = Object.keys(charMapB);
-
-    if (keysA.length === keysB.length) {
-        return keysA.every((keyA) => {
-            const keyB = keysB.find((keyB) => keyB === keyA)
-            if (keyB && (charMapA[keyA] === charMapB[keyB])) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-    } else {
-        return false
-    }
+    return cleanString(stringA) === cleanString(stringB);
 }
+// =========================================================
 
 console.log(anagrams('rail safety', 'fairy tales'));
 console.log(anagrams('RAIL! SAFETY!', 'fairy tales'));
