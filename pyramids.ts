96f4902db1f9865b5265cf12ea0710f633163320
Prashant Yadav <41838500+prashantyadav1397@users.noreply.github.com>
2025-06-03 11:10:07 +0530

    Update pyramids.ts

--- pyramids.ts
+++ pyramids.ts
@@ -31,22 +31,47 @@
 // ================================================================
 
 // Approach 2 =====================================================
-const pyramid = (n: number) => {
-    let mid: number = Math.floor((2 * (n - 1)) / 2);
+// const pyramid = (n: number) => {
+//     const mid: number = Math.floor((2 * (n - 1)) / 2);
 
-    for (let row = 0; row < n; row++) {
-        let str: string = '';
+//     for (let row = 0; row < n; row++) {
+//         let str: string = '';
 
-        for (let col = 0; col < 2 * n - 1; col++) {
-            if (mid - row <= col && mid + row >= col) {
-                str += '#';
-            } else {
-                str += ' ';
-            }
-        }
+//         for (let col = 0; col < 2 * n - 1; col++) {
+//             if (mid - row <= col && mid + row >= col) {
+//                 str += '#';
+//             } else {
+//                 str += ' ';
+//             }
+//         }
 
-        console.log(str);
+//         console.log(str);
+//     }
+// }
+// ================================================================
+
+// Approach 3 =====================================================
+const pyramid = (n: number, row: number = 0, str: string = '') => {
+    if (row === n) {
+        return;
     }
+
+    if (str.length === 2 * n - 1) {
+        console.log(str);
+
+        return pyramid(n, row + 1);
+    }
+
+    const mid: number = Math.floor((2 * (n - 1)) / 2);
+    let character: string = '';
+
+    if (mid - row <= str.length && mid + row >= str.length) {
+        character = '#';
+    } else {
+        character = ' ';
+    }
+
+    return pyramid(n, row, str + character);
 }
 // ================================================================
 
